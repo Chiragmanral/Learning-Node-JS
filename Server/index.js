f04c705be38f22835ca4e6393f9ef3cedc6f8a87
Chirag Singh Manral <chiragmanral2003@gmail.com>
2025-05-22 13:23:19 +0530

    new code

--- Server/index.js
+++ Server/index.js
@@ -1,54 +1,15 @@
 // const http = require("node : http");
 const http = require("http");
-const fs = require("fs");
-const url = require("url");
 const express = require('express');
 
 const app = express();
 
-const myServer = http.createServer((req, res) => {
-    if(req.url === '/favicon.ico') return res.end();
-    const log = `${Date.now()} : ${req.url} : New Req Received\n`;
-    const myUrl = url.parse(req.url, true);
-    console.log(myUrl);
-    fs.appendFile("log.txt", log, () => {
-        switch (myUrl.pathname) {
-            case '/' : 
-                if(req.method === 'GET') res.end("Welcome to the Homepage");          
-                break;
-
-            case '/about-us' :
-                const username = myUrl.query.name;
-                if(req.method === 'GEt') res.end(`I am ${username}`);
-                break;
-
-            case '/buy-now' :
-                res.end("Pay the price");
-                break;
-
-            case '/hello' :
-                res.end("Hello Bro");
-                break;
-
-            case '/search' : 
-                const searchQuery = myUrl.query.search_query;
-                res.end(`Here are your results for ${searchQuery}`);
-                break;
-
-            case '/signup' :
-                if(req.method === 'GET') res.end("This is your signup page");
-                if(req.method === 'POST') {
-                    // DB Query
-                    res.end("You are registered successfully!!");
-                }
-                break;
-                
-            default :
-                res.end("404 Not found");
-        }
-    })
+app.get('/', (req, res) => {
+    return res.send("Hello from Home Page");
 });
 
-myServer.listen(8000, () => {
-    console.log("Server is started successfully");
-})
+app.get('/about', (req, res) => {
+    return res.send("Hello from About Page " + req.query.name + " your age is :- " + req.query.age);
+});
+
+app.listen(8000, () => console.log('Server started!!'))
