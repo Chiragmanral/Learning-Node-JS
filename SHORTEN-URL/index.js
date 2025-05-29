293eb210c29ebd61ef6b9fabe7d0e058428cc3b7
Chirag Singh Manral <chiragmanral2003@gmail.com>
2025-05-29 13:23:33 +0530

    new code

--- SHORTEN-URL/index.js
+++ SHORTEN-URL/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const urlRoute = require("./routes/url");
 const { connectToMongoDb } = require("./connection");
+const URL = require("./models/url");
 
 const app = express();
 const PORT = 8000;
@@ -8,8 +9,23 @@ const PORT = 8000;
 connectToMongoDb("mongodb://127.0.0.1:27017/short-url")
 .then(() => console.log("MongoDB connected successfully!!"))
 .catch(err => console.log("Error in connecting with database", err));
+ 
+app.use(express.json());
 
 app.use("/url", urlRoute);
+
+app.get("/:shortId", async (req, res) => {
+     const shortId = req.params.shortId;
+     const entry = await URL.findOneAndUpdate({ shortId }, {
+          $push : {
+               visitHistory : {
+                    timestamp : Date.now()
+               }
+          }
+     }
+     )
+     res.redirect(entry.redirectURL);
+})
 
 app.listen(PORT, () => {
      console.log(`Server started at PORT: ${PORT}`);
