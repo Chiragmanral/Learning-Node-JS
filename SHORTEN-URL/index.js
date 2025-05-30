7df8bde8e26d464329d6c2870f068adc512b0a5d
Chirag Singh Manral <chiragmanral2003@gmail.com>
2025-05-30 13:27:23 +0530

    new code

--- SHORTEN-URL/index.js
+++ SHORTEN-URL/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const path = require("path");
 const urlRoute = require("./routes/url");
+const staticRoute = require("./routes/staticRouter");
 const { connectToMongoDb } = require("./connection");
 const URL = require("./models/url");
+const { homedir } = require("os");
 
 const app = express();
 const PORT = 8000;
@@ -9,14 +12,15 @@ const PORT = 8000;
 connectToMongoDb("mongodb://127.0.0.1:27017/short-url")
 .then(() => console.log("MongoDB connected successfully!!"))
 .catch(err => console.log("Error in connecting with database", err));
- 
-app.use(express.json());
 
-app.get("/test", (req, res) => {
-     return res.end(`<h1>Hey from server</h1>`);
-})
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended : false }))
 
 app.use("/url", urlRoute);
+app.use("/", staticRoute);
 
 app.get("/:shortId", async (req, res) => {
      const shortId = req.params.shortId;
@@ -28,7 +32,12 @@ app.get("/:shortId", async (req, res) => {
           }
      }
      )
-     res.redirect(entry.redirectURL);
+     if(entry.redirectURL !== null) {
+          res.redirect(entry.redirectURL);
+     }
+     else {
+          res.end("Redirect URL does not exist!!");
+     }
 })
 
 app.listen(PORT, () => {
