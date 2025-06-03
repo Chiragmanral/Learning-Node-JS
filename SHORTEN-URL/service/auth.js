8ca6c6114f839b6cbbf5356e1331dbd7d2b446c5
Chirag Singh Manral <chiragmanral2003@gmail.com>
2025-06-03 13:32:22 +0530

    new code

--- SHORTEN-URL/service/auth.js
+++ SHORTEN-URL/service/auth.js
@@ -1,14 +1,25 @@
-const sessionIdToUserMap = new Map();
+const jwt = require("jsonwebtoken");
+const secret = "CHIRAG$123@987";
 
-function setUser(id, user) {
-    sessionIdToUserMap.set(id, user);
+function setUser(user) {
+//   const payload = { id: user._id.toString() };
+  return jwt.sign({
+    _id : user._id,
+    email : user.email,
+    password : user.password
+  }, secret);
 }
 
-function getUser(id) {
-    return sessionIdToUserMap.get(id);
+function getUser(token) {
+  if (!token) return null;
+  try {
+    return jwt.verify(token, secret);
+  } catch (e) {
+    return null; 
+  }
 }
 
 module.exports = {
-    setUser,
-    getUser
+  setUser,
+  getUser,
 };
